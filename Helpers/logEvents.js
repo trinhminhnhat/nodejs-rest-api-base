279988cedbd96dbcdcc98adb580134493484b5cb
Trịnh Minh Nhật <trinhmnhat@gmail.com>
2022-10-18 20:18:08 +0700

    feat: write log to file

---
+++ Helpers/logEvents.js
@@ -0,0 +1,17 @@
+const fs = require('fs').promises;
+const path = require('path');
+const { format } = require('date-fns');
+
+const logEvents = async (message) => {
+    const fileName = path.join(__dirname, '../Logs', `${format(new Date(), 'dd-MM-yyyy')}-errors.log`);
+    const dateTime = `${format(new Date(), 'dd-MM-yyyy HH:mm:ss')}`;
+    const logContent = `${dateTime} - ${message}\n`;
+
+    try {
+        fs.appendFile(fileName, logContent);
+    } catch (error) {
+        console.log('error: ', error);
+    }
+};
+
+module.exports = logEvents;
--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const createError = require('http-errors');
+const { v4: uuid } = require('uuid');
 
 const db = require('./config/db');
 const userRoute = require('./Routes/User.route');
+const logEvents = require('./Helpers/logEvents');
 
 require('dotenv').config();
 
@@ -15,8 +17,7 @@ app.use(morgan('common'));
 
 const PORT = process.env.PORT || 5000;
 
-
-db.connect();
+// db.connect();
 // mongoose.connect('mongodb://127.0.0.1:27017', (err) => {
 //     if (err) {
 //         console.error('error: ', err);
@@ -27,7 +28,9 @@ db.connect();
 
 app.use('/v1/', userRoute);
 
+// handle when call api is not existed
 app.use((req, res, next) => {
+    // Level 1
     // res.status(404);
     // res.json({
     //     status: 404,
@@ -37,15 +40,18 @@ app.use((req, res, next) => {
     //     }
     // })
 
+    // Level 2 using the http-errors library
     next(createError(404, 'Not Found'));
 });
 
+// handle when api has errors
 app.use((err, req, res, next) => {
+    logEvents(`errorId: ${uuid()} - ${req.url} - ${req.method} ${err.message}`);
     res.status(err.status || 500);
     res.json({
         status: err.status || 500,
         message: err.message,
-    })
+    });
 });
 
 app.listen(PORT, () => {
